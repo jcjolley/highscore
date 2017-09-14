787fbe6181c4079bf69232acc55bbdf5ae6488d2
Cory Owens <cory.r.owens@gmail.com>
2017-09-13 21:55:59 -0600

    Fixed syntax.

--- test.py
+++ test.py
@@ -55,9 +55,9 @@ def run():
     # Server settings
     # Choose port 8080, for port 80, which is normally used for a http server, you need root access
     server_address = ('127.0.0.1', 8081)
-    httpd = HTTPServer(server_address, highScore_RequestHandler)
+    httpd = HTTPServer(server_address, HighScoreRequestHandler)
     print('running server...')
     httpd.serve_forever()
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     run()
