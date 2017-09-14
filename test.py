478eca81d4db37669665694abdbd18aff6fa2830
Joshua Jolley <jolleyboy@gmail.com>
2017-09-13 19:38:05 -0600

    Starting point for a simple web server

--- test.py
+++ test.py
@@ -1,1 +1,46 @@
-print('Test');
+#!/usr/bin/env python
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib
+
+# HTTPRequestHandler class
+
+
+class highScore_RequestHandler(BaseHTTPRequestHandler):
+
+  # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+    def do_POST(self):
+        length = int(self.headers['Content-Length'])
+        post_data = urllib.parse.parse_qs(
+            self.rfile.read(length).decode('utf-8'))
+        # You now have a dictionary of the post data
+        print('POST data: ', post_data)
+        self.wfile.write("Lorem Ipsum".encode("utf-8"))
+
+
+def run():
+    print('starting server...')
+
+    # Server settings
+    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+    server_address = ('127.0.0.1', 8081)
+    httpd = HTTPServer(server_address, highScore_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+
+
+run()
